5a55b2d6e9355cf64ef2b76b9b95d032c7f53f1f
John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>
2019-09-18 13:27:39 +0200

    Pride 🌈
    Signed-off-by: John Molakvoæ (skjnldsv) <skjnldsv@protonmail.com>

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pride
+Show your pride to the world and to your nextcloud instance!
+
+Place this app in **nextcloud/apps/**
---
+++ appinfo/app.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * This file is part of the Unsplash App
+ * and licensed under the AGPL.
+ */
+use OCP\Util;
+
+Util::addStyle('pride', 'style');
