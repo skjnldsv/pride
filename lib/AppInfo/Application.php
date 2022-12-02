8c38346dac3e959f85306ba3c8e64e43ddb2e842
John Molakvoæ <skjnldsv@protonmail.com>
2022-12-02 14:54:33 +0100

    Fix for 25 and above
    Signed-off-by: John Molakvoæ <skjnldsv@protonmail.com>

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -11,16 +11,25 @@ use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
+use OCP\IConfig;
 
 class Application extends App implements IBootstrap {
+	const APP_ID = 'pride';
+
 	public function __construct(array $urlParams = []) {
-		parent::__construct('pride', $urlParams);
+		parent::__construct(self::APP_ID, $urlParams);
 	}
 
 	public function register(IRegistrationContext $context): void {
+		/**
+		 * Enforce primary colour to yellow
+		 * @var IConfig $config
+		 */
+		$config = \OC::$server->get(IConfig::class);
+		$config->setAppValue('theming', 'color', '#E4D56F');
 	}
 
 	public function boot(IBootContext $context): void {
-		Util::addStyle('pride', 'style');
+		Util::addStyle(self::APP_ID, 'style');
 	}
 }
